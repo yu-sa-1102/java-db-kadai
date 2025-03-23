2ea2185e2f4a31a57f235ca4c67615b0705f78c9
yu-sa-1102 <spica-yihsa3@i.softbank.jp>
2025-03-23 19:40:15 +0900

    kadai_010を更新する(レビュー後)

--- java_study/src/kadai_010/Scores_Chapter10.java
+++ java_study/src/kadai_010/Scores_Chapter10.java
@@ -24,13 +24,13 @@ public class Scores_Chapter10 {
 			
 			// SQLクエリを準備
 			statement = con.createStatement();
-			String sql = "UPDATE score SET score_math = 95, score_english = 80 WHERE id = 5; ";
+			String sql = "UPDATE scores SET score_math = 95, score_english = 80 WHERE id = 5; ";
 			
 			// SQLクエリを実行（DBMSに送信）
 			int rowCnt = statement.executeUpdate(sql);
 			System.out.println(rowCnt + "件のレコードが更新されました。");
 			
-			String order = "SELECT id, name, score_math, score_english FROM score ORDER BY score_math DESC, score_english DESC;";
+			String order = "SELECT id, name, score_math, score_english FROM scores ORDER BY score_math DESC, score_english DESC;";
 			ResultSet result = statement.executeQuery(order);	
 			System.out.println("数学・英語の点数が高い順に並べ替えました");
 			while(result.next()) {
